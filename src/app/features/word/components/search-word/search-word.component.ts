d5340035bafb0ce98abd43ad1c1195dbc7a3fa32
Juan Hamilton <hedwards507@gmail.com>
2023-02-02 00:55:41 -0500

    Add pronunciation playback

--- src/app/features/word/components/search-word/search-word.component.ts
+++ src/app/features/word/components/search-word/search-word.component.ts
@@ -13,11 +13,23 @@ export class SearchWordComponent {
   @Output() search = new EventEmitter<SearchQuery>();
 
   isError = false;
+  audio: HTMLAudioElement;
+
+  constructor() {
+    this.audio = new Audio();
+  }
 
   onSearch({ query }: SearchQuery) {
     const formattedWord = query.trim();
 
     this.isError = formattedWord.length === 0;
     this.search.emit({ query: formattedWord });
+  }
+
+  playAudio() {
+    if (!this.audio.paused || !this.audioSrc) return;
+
+    this.audio.src = this.audioSrc;
+    this.audio.play();
   }
 }
