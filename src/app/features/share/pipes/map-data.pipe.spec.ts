4b405e2eb6151208e477ad5fb2cc227554657a91
Juan Hamilton <hedwards507@gmail.com>
2023-02-02 22:10:04 -0500

    Create map data pipe

---
+++ src/app/features/share/pipes/map-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MapDataPipe } from './map-data.pipe';
+
+describe('MapDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MapDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
