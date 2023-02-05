3a7e11a093a3aca5e2543a517518c628eae700f0
Juan Hamilton <hedwards507@gmail.com>
2023-02-05 07:27:45 -0500

    Create tests for ThemeService

--- src/app/features/share/services/font.service.spec.ts
+++ src/app/features/share/services/font.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
+import { CookieService } from 'ngx-cookie-service';
+import { first } from 'rxjs';
+import { COOKIE_FONT } from '../../../core/constants/cookies';
 
-import { FontService } from './font.service';
+import { Font, FontService } from './font.service';
 
 describe('FontService', () => {
   let service: FontService;
+  let cookieServiceSpy: jasmine.SpyObj<CookieService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('CookieService', ['get', 'set']);
+
+    TestBed.configureTestingModule({
+      providers: [FontService, { provide: CookieService, useValue: spy }],
+    });
+
     service = TestBed.inject(FontService);
+    cookieServiceSpy = TestBed.inject(
+      CookieService
+    ) as jasmine.SpyObj<CookieService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#changeFont should set selected font', () => {
+    service.changeFont(Font.SANS_SERIF);
+    service.font$.pipe(first()).subscribe((font) => {
+      expect(font)
+        .withContext('font should be SANS_SERIF after selecting that font')
+        .toBe(Font.SANS_SERIF);
+    });
+
+    service.changeFont(Font.SERIF);
+    service.font$.pipe(first()).subscribe((font) => {
+      expect(font)
+        .withContext('font should be SERIF after selecting that font')
+        .toBe(Font.SERIF);
+    });
+
+    service.changeFont(Font.MONO);
+    service.font$.pipe(first()).subscribe((font) => {
+      expect(font)
+        .withContext('font should be MONO after selecting that font')
+        .toBe(Font.MONO);
+    });
+  });
+
+  it('#changeFont should set COOKIE_FONT to be selected font', () => {
+    cookieServiceSpy.get.and.returnValue('mono');
+    service.changeFont(Font.MONO);
+    service.font$.pipe(first()).subscribe(() => {
+      expect(cookieServiceSpy.get(COOKIE_FONT))
+        .withContext("cookie should be set to be 'mono'")
+        .toBe(Font.MONO);
+    });
+
+    cookieServiceSpy.get.and.returnValue('serif');
+    service.changeFont(Font.SERIF);
+    service.font$.pipe(first()).subscribe(() => {
+      expect(cookieServiceSpy.get(COOKIE_FONT))
+        .withContext("cookie should be set to be 'serif'")
+        .toBe(Font.SERIF);
+    });
   });
 });
--- src/app/features/share/services/font.service.ts
+++ src/app/features/share/services/font.service.ts
@@ -7,21 +7,21 @@ import { COOKIE_FONT } from '../../../core/constants/cookies';
   providedIn: 'root',
 })
 export class FontService {
-  font: BehaviorSubject<Font>;
+  font$: BehaviorSubject<Font>;
 
   constructor(private cookieService: CookieService) {
     const fontCookie = this.cookieService.get(COOKIE_FONT);
-    this.font = new BehaviorSubject<Font>(
+    this.font$ = new BehaviorSubject<Font>(
       fontCookie ? (fontCookie as Font) : Font.SANS_SERIF
     );
   }
 
   changeFont(font: Font) {
-    this.font.next(font);
+    this.font$.next(font);
     this.setCookie(font);
   }
 
-  setCookie(font: Font) {
+  private setCookie(font: Font) {
     this.cookieService.set(COOKIE_FONT, font);
   }
 }
