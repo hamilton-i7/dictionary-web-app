018c9b5331e32c5523ad52d5fcf6ca2feb9f7908
Juan Hamilton <hedwards507@gmail.com>
2023-02-05 10:32:22 -0500

    Add tests for WordMeaningComponent

--- src/app/features/word/components/word-meaning/word-meaning.component.spec.ts
+++ src/app/features/word/components/word-meaning/word-meaning.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { WordMeaningComponent } from './word-meaning.component';
 
@@ -8,16 +10,47 @@ describe('WordMeaningComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WordMeaningComponent ]
-    })
-    .compileComponents();
+      declarations: [WordMeaningComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(WordMeaningComponent);
     component = fixture.componentInstance;
+
+    component.partOfSpeech = 'noun';
+    component.synonyms = ['telephone', 'mobile', 'cell'];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display #partOfSpeech in lowercase', () => {
+    const titleDe = fixture.debugElement.query(
+      By.css('.part-of-speech__title')
+    );
+    const titleEl: HTMLHeadingElement = titleDe.nativeElement;
+    const expectedWord = component.partOfSpeech.toLowerCase();
+
+    expect(titleEl.textContent)
+      .withContext('should display provided #partOfSpeech in lowercase')
+      .toContain(expectedWord);
+  });
+
+  it("should compose synonym link using the synonym's name", () => {
+    const synonymLinkDe = fixture.debugElement.queryAll(
+      By.css('.synonyms__list-item-link')
+    );
+    synonymLinkDe.forEach((debugEl) => {
+      const synonymLinkEl: HTMLAnchorElement = debugEl.nativeElement;
+
+      expect(component.synonyms.includes(synonymLinkEl.textContent || ''))
+        .withContext('should exist in synonyms array')
+        .toBeTrue();
+      expect(synonymLinkEl.href)
+        .withContext('should be dynamically composed using the synonym')
+        .toContain(`/browse/${synonymLinkEl.textContent}`);
+    });
   });
 });
